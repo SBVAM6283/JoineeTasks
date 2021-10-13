3c4ef7038ce653a39e8e066348fd51eeffa9f4a8
SBVAM6283 <92308638+SBVAM6283@users.noreply.github.com>
2021-10-13 09:11:05 +0530

    Add files via upload
    App component for calc App

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { ResultComponent } from './result/result.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculatorComponent,
+    ResultComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
